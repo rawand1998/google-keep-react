555c9a5386aeee265b7e65b63df30f5ceccf550d
rawand1998 <rawandgaradh@gmail.com>
2022-06-21 11:19:30 +0300

    header component and styling

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,7 +1,10 @@
 import React from "react";
 function Header (){
     return(
-        <div></div>
+        <div className="header-container">
+            <img src="https://www.gstatic.com/images/branding/product/1x/keep_2020q4_48dp.png" alt="Image not found"/>
+            <h1>Keep</h1>
+        </div>
     )
 }
 export default Header
