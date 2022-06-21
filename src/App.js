51ff91e69cb26604d6fe74ec1312cad496f71fec
rawand1998 <rawandgaradh@gmail.com>
2022-06-21 11:11:12 +0300

    create and delete post

--- src/App.js
+++ src/App.js
@@ -1,14 +1,37 @@
-import logo from './logo.svg';
+import React,{useState} from 'react';
 import './App.css';
 import Notes from './Components/Notes'
 import CreateNotes from './Components/CreateNotes'
 import Header from './Components/Header'
-function App() {
+function App(props) {
+  const [notes, setNotes] = useState([]);
+
+  function addNote(newNote) {
+    setNotes((prevValue) => {
+      return [...prevValue, newNote];
+    });
+  }
+
+  function deleteNotes(id) {
+    setNotes((preValue) => {
+      return [...preValue.filter((note, index) => index !== id)];
+    });
+  }
   return (
     <div className="App">
-     <Header />
-     <CreateNotes />
-     <Notes />
+      <Header />
+    
+      <CreateNotes onAdd={addNote} />
+      {notes.map((note, index) => (
+        <Notes
+          key={index}
+          id={index}
+          title={note.title}
+          content={note.content}
+          onDelete={deleteNotes}
+        />
+      ))}
+    
     </div>
   );
 }
